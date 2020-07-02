50c81ae343388207916cb8228e8a8ee69d5bb0a0
Jimmy Starling <jimmystarlingdev@gmail.com>
2020-07-01 21:58:00 -0300

    Add material icons.

--- src/components/TextFields.js
+++ src/components/TextFields.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+
+import CloseIcon from '@material-ui/icons/Close';
+import MicIcon from '@material-ui/icons/Mic';
+import CreateIcon from '@material-ui/icons/Create';
 
 import { useStyles } from '../styles/global';
+import { Box } from '@material-ui/core';
 
 export const TextFields = (props) => {
     var items = props.items;
@@ -10,14 +16,42 @@ export const TextFields = (props) => {
 
     return (
         <div className={classes.textFields} >
+            <Box>
+                <IconButton aria-label="delete">
+                    <CloseIcon />
+                </IconButton >
+            </Box>
             <TextField
                 fullWidth
                 id={`text-${items.lang}`} 
                 className={classes.textField}
                 placeholder={items.text}
-                InputProps={{ disableUnderline: true }}
+                InputProps={{ 
+                    classes: {
+                        input: classes.textFieldResize
+                    }, 
+                    disableUnderline: true, 
+                }}
+                InputLabelProps={{
+                    classes: {
+                        root: classes.textFieldResize
+                    }
+                }}
                 multiline
             />
+            <Box display="flex">
+                <Box width="50%">
+                    <IconButton aria-label="delete">
+                        <MicIcon />
+                    </IconButton >
+                </Box>
+                <Box width="50%">
+                    <label> 0/100 </label>
+                    <IconButton aria-label="delete">
+                        <CreateIcon />
+                    </IconButton >
+                </Box>
+            </Box>
         </div>
     )
 }
