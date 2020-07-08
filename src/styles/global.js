5d916afc79cde1407198c9015faf4b020cc8edad
Jimmy Starling <jimmystarlingdev@gmail.com>
2020-07-08 18:11:17 -0300

    Add styles to be used by Textfields

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,4 +1,5 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
 
 export const useStyles = makeStyles((theme) => ({
     root: {
@@ -48,11 +49,36 @@ export const useStyles = makeStyles((theme) => ({
 	textField:{
 	},
 	textFieldResize: {
-		fontSize: 62,
-		fontWeight: 700,
+		fontSize: 25,
+		fontWeight: 400,
 	},
 	textcontainer:{
 		height: '350px',
+	},
+	formControl: {
+		margin: theme.spacing(1),
+		minWidth: 120,
+	},
+	selectInput: {
+		input: {
+			border: 'none',
+			'&:focus': {
+				backgroundColor: 'transparent',
+			},
+		}
+	},
+	selectEmpty: {
+		marginTop: theme.spacing(2),
+	},
+	inputCloseContainer:{
+		position: 'absolute',
+		top: '0px',
+		right: '0px',
+		zIndex: 3
+	},
+	inputConfigContainer: {
+		position: 'absolute',
+		bottom: '0px'
 	},
 	tabs:{
 		borderBottom: '1px solid rgba(0,0,0, 0.6)',
@@ -85,3 +111,38 @@ export const flexContainer = {
 	justifyContent: 'center',
 	padding: 0,
 };
+
+export const BootstrapInput = withStyles((theme) => ({
+	root: {
+	  'label + &': {
+		marginTop: theme.spacing(3),
+	  },
+	},
+	input: {
+	  borderRadius: 4,
+	  position: 'relative',
+	  backgroundColor: theme.palette.background.paper,
+	  border: '1px solid #ced4da',
+	  fontSize: 16,
+	  padding: '10px 26px 10px 12px',
+	  transition: theme.transitions.create(['border-color', 'box-shadow']),
+	  // Use the system font instead of the default Roboto font.
+	  fontFamily: [
+		'-apple-system',
+		'BlinkMacSystemFont',
+		'"Segoe UI"',
+		'Roboto',
+		'"Helvetica Neue"',
+		'Arial',
+		'sans-serif',
+		'"Apple Color Emoji"',
+		'"Segoe UI Emoji"',
+		'"Segoe UI Symbol"',
+	  ].join(','),
+	  '&:focus': {
+		borderRadius: 4,
+		borderColor: '#80bdff',
+		boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+	  },
+	},
+}))(InputBase);
