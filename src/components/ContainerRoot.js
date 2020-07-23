3c7daf7866b0c78f0068b7ceecb252c0c779f025
Jimmy Starling <jimmystarlingdev@gmail.com>
2020-07-23 20:33:10 -0300

    Rename variables and functions.

--- src/components/ContainerRoot.js
+++ src/components/ContainerRoot.js
@@ -1,92 +1,81 @@
 import React from 'react';
+// Core components
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+// Styles
 import { useStyles } from '../styles/global'; 
-
+// External components
 import { TextFields } from './TextFields';
 import { Headbar } from './Headbar';
 
 export default function ContainerRoot() {
-	/*const languages = [
-		{
-		id: 0,
-		lang: 'english', 
-		}, 
-		{
-		id: 1,
-		lang: 'portuguese', 
-		},
-		{
-		id: 2,
-		lang: 'spanish', 
-		}, 
-		{
-		id: 3,
-		lang: 'japanese', 
-		}, 
-	];*/
-
-	const selected_languages = [
+	// Languages dictionary
+	const languages = [
 		{
 			id: 0,
 			lang: 'Node.js', 
 			text: '',
-			output: false, // !IMPORTANT Utilizado na logica de tradução.
+			output: false, // !IMPORTANT Utilizado with the translation logic.
 			input: true
 		}, 
 		{
 			id: 1,
 			lang: 'PHP', 
 			text: 'Tradução',
-			output: true, // !IMPORTANT Utilizado na logica de tradução.
+			output: true, // !IMPORTANT Utilized with the translation logic.
 			input: false
 		}
 	];
 
-	const [itemsValue, setItemsValue] = React.useState(selected_languages);
+	const [languageValue, setLanguageValue] = React.useState(languages);
 	const classes = useStyles();
 
-	const SpaceDynamic = (index, space) => {
-		if(index !== 0) {
-			return <Box px={space}></Box>;
+	const FlexibleDivider = (index) => {
+		while(index === 0){
+			return(
+			<Divider 
+				className={classes.divider} 
+				orientation="vertical" 
+				flexItem 
+			/>);
 		}
 	};
-	
-	const ListDynamic = (index) => {
-		if (index === 0) {
-		  	return <Divider 
-						className={classes.divider} 
-						orientation="vertical" 
-						flexItem 
-					/>;
-		}
-	}
 
 	return (
 		<div className={classes.root}>
 		<Grid container direction="column" spacing={3}>
+			<Grid container className={classes.textFieldTitleContainer}>
+				<Typography variant="h3" gutterBottom>
+					Translate Code
+					<Typography variant="h6" gutterBottom>
+						Easily
+					</Typography>
+				</Typography>
+			</Grid>
 			<Grid container className={classes.textFieldContainer} justify="center" spacing={0}>
 				<Paper 
 					className={`${classes.paper} ${classes.spacingPaper}`} 
 					elevation={4}>
 
-					<Headbar items={itemsValue}/>
+					<Headbar items={languageValue}/>
 					<List className={`${classes.flexContainer} ${classes.listContainer}`}>
-						{itemsValue.map((value, index) => (
-							<ListItem 
-								key={value.id} 
-								className={classes.listItem}>
-								<TextFields 
-									order={index}
-									items={value}
-									ids={value.id}/>
-								{ ListDynamic(index) }
-							</ListItem>
-						))}
+						{ 
+							languageValue.map((value, index) => (
+								<ListItem 
+									key={value.id} 
+									className={classes.listItem}>
+									<TextFields 
+										position={index}
+										items={value}
+										ids={value.id}/>
+									{ FlexibleDivider(index) }
+								</ListItem>
+							)) 
+						}
 					</List>
 
 				</Paper>
