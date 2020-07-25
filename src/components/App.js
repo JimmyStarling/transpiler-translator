311483496f9b0218f347d4c50db333880c6d477f
Jimmy Starling <jimmystarlingdev@gmail.com>
2020-07-24 21:56:45 -0300

    rename ContainerRoot to AppLayout

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import ContainerRoot from './ContainerRoot';
+import AppLayout from './AppLayout';
 
 function App() {
   return (
     <div className="App">
-      <ContainerRoot/>
+      <AppLayout/>
     </div>
   );
 }
