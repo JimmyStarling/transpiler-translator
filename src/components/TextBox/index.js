a6a77bbe42443c49724db040133169d84e927a66
Jimmy Starling <jimmystarlingdev@gmail.com>
2020-07-25 19:27:25 -0300

    move tools components to TextBox folder.

--- src/components/TextBox/index.js
+++ src/components/TextBox/index.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import TextField from '@material-ui/core/TextField';
-import IconButton from '@material-ui/core/IconButton';
-
+import { Grid,
+         Box, 
+         FormControl,
+         TextField,
+         IconButton, 
+         Select, 
+         MenuItem, 
+         FormGroup } from '@material-ui/core';
+// Icons
 import CloseIcon from '@material-ui/icons/Close';
 import MicIcon from '@material-ui/icons/Mic';
 import CreateIcon from '@material-ui/icons/Create';
 import KeyboardIcon from '@material-ui/icons/Keyboard';
-
+// Styles
 import { useStyles } from '../../styles/global';
-import { Box, FormControl, Select, MenuItem, FormGroup } from '@material-ui/core';
 import { BootstrapInput } from '../../styles/global';
-import Grid from '@material-ui/core/Grid';
+// Custom components
+import { HeaderTools } from './components/HeaderTools';
+import { FooterTools } from './components/FooterTools';
 
 export const TextBox = (props) => {
     /** 
@@ -20,121 +27,30 @@ export const TextBox = (props) => {
      * 
      **/
     var items = props.items;
-    var item_position = props.position;
+    var textbox_index = props.position;
     var language = String(items.lang);
     // Classes from global.js
     const classes = useStyles();
     // State hooks variable. 
     const [textFieldValue, setTextFieldValue] = React.useState('');
-    const [icon, setIcon] = React.useState();
-    // This func will clear the selected textfield strings from state.
-    const _handleClearText = () => {
+    
+    // This will clear the selected textfield.
+    const _setTextClear = () => {
         setTextFieldValue('');
     };
-    // This func return a icon component to ControlLabel according to the <Select/> into ControlLabel props(e.g 'control={here}').
-    const _handleSelect = (e) => {
-        var current_icon = e.target.value;
-        setIcon(current_icon);
-    };
-    // This func set textField value 
-    const _handleText = (e) => {
+    const _setTextValue = (e) => {
         var text = e.target.value;
         setTextFieldValue(text);
     };
-    const CustomIcon = () => {
-        var current_icon = icon;
-        if(current_icon === 10){
-            return  <IconButton 
-                        className={classes.iconButton} 
-                        aria-label="Write" 
-                        component="span"
-                        onClick={() => {return alert('Testing :)');}}>
-                        <CreateIcon/>
-                    </IconButton>;
-        }else if(current_icon === 20){
-            return  <IconButton className={classes.iconButton} aria-label="Virtual Keyboard - DVORAK" component="span">
-                        <KeyboardIcon/>
-                    </IconButton>;
-        }else if(current_icon === 30){
-            return  <IconButton className={classes.iconButton} aria-label="Virtual Keyboard" component="span">
-                        <KeyboardIcon/>
-                    </IconButton>;
-        } else {
-            return <IconButton className={classes.iconButton} aria-label="upload picture" component="span" onClick={() => {return alert('hello :)');}}>
-                        <CreateIcon/>
-                    </IconButton>;
-        }
-    };
-
-    // Theses two functions will return the components
-    // only when it's the first TextField.
-    const BottomTools = () => { 
-        if(item_position === 0){
-            return  <Box className={classes.bottomTools} display="flex">
-                        <Box className={classes.inputMicContainer} width="auto">
-                            <IconButton aria-label="delete">
-                                <MicIcon />
-                            </IconButton >
-                        </Box>
-                        {/** 
-                        *   This components bellow allow users 
-                        *   to use virtual keyboards, handwriting and etc..  
-                        */}
-                        <Box width="auto" className={classes.inputConfigContainer}>
-                            <label> {textFieldValue.length}/100 </label>
-                            <FormControl component="fieldset">
-                                <FormGroup>
-                                    {/** 
-                                    *     This component identify which value it's selected 
-                                    *     from <Select/> component and return a Icon 
-                                    */}
-                                    <Box className={classes.inputSelect}>
-                                    
-                                    <CustomIcon/>
-                                    <Select
-                                        labelId="demo-simple-select-label"
-                                        onChange={_handleSelect}
-                                        input={<BootstrapInput />}
-                                        boxShadow={0}
-                                    >
-                                        <MenuItem value={10}>
-                                            <CreateIcon/>
-                                            <label>Inglês</label>
-                                        </MenuItem>
-                                        <MenuItem value={20}>
-                                            <KeyboardIcon/>
-                                            <label>Inglês DVORAK</label>
-                                        </MenuItem>
-                                        <MenuItem value={30}>
-                                            <KeyboardIcon/>
-                                            <label>Inglês - Pesquisa escrita</label>
-                                        </MenuItem>
-                                    </Select>
-                                    </Box>
-                                </FormGroup>
-                            </FormControl>
-                        </Box>
-                    </Box>;
-        }
-    }
-    const TopTools = () => { 
-        if(item_position === 0){
-            return  <Box className={classes.inputCloseContainer}>
-                        <IconButton aria-label="delete" onClick={_handleClearText}>
-                            <CloseIcon />
-                        </IconButton >
-                    </Box>
-        }
-    }
 
     return (
         <div className={`${classes.textFields} ${classes.sourceWrap}`}>
             <Grid className={classes.sourceInput}>
                 <Grid className={classes.inputWrap}>
-                    { TopTools }
+                    <HeaderTools eventListener={_setTextClear} textboxIndex={textbox_index} />
                     <TextField
                         fullWidth
-                        id={`text-${items.lang}`} 
+                        id={`text-${language}`} 
                         className={classes.textField}
                         placeholder={items.text}
                         InputProps={{ 
@@ -150,12 +66,12 @@ export const TextBox = (props) => {
                         }}
                         defaultValue={items.text}
                         value={textFieldValue}
-                        onChange={_handleText}
+                        onChange={_setTextValue}
                         inputProps={{ maxLength: 100 }}
                         multiline
                     />
                 </Grid>
-                { BottomTools }
+                <FooterTools textboxValue={textFieldValue} textboxIndex={textbox_index}/>
             </Grid>
         </div>
     );
