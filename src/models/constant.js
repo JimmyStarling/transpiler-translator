6205a53098cf275aa172e25e04736b2c7123706a
Jimmy Starling <jimmystarlingdev@gmail.com>
2020-07-25 19:34:02 -0300

    add fake language data.

---
+++ src/models/constant.js
@@ -0,0 +1,17 @@
+// Languages dictionary
+export const languages = [
+    {
+        id: 0,
+        lang: 'Node.js', 
+        text: '',
+        output: false, // !IMPORTANT Utilizado with the translation logic.
+        input: true
+    }, 
+    {
+        id: 1,
+        lang: 'PHP', 
+        text: 'Tradução',
+        output: true, // !IMPORTANT Utilized with the translation logic.
+        input: false
+    }
+];
